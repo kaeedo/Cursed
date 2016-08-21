a22d8b1b3cdfb9070007d07388d896eea70b5599
Kai Ito <kai.ito@gmail.com>
2016-08-21 12:32:28 +0200

    Use PCLStorage to download and save file

--- Cursed.Base/Modpack.fs
+++ Cursed.Base/Modpack.fs
@@ -1,8 +1,8 @@
 ﻿namespace Cursed.Base
 
 open System
-open System.Text
-//open HttpFs.Client
+open FSharp.Data
+open PCLStorage
 
 type State =
     { Link: string 
@@ -20,11 +20,17 @@ type Modpack() =
     member this.DownloadZip(link: string) =
         let modpackLink = if link.EndsWith("/", StringComparison.OrdinalIgnoreCase) then link.Substring(0, link.Length) else link
         let fileUrl = modpackLink + "/files/latest"
-        (*let request =
-            Request.createUrl Get fileUrl
-            |> Request.autoFollowRedirectsDisabled
-        let response =
-            job {
-                use! response = getResponse request
-            }*)
+        
+        async {
+            let! response = Http.AsyncRequestStream(fileUrl)
+
+            let rootFolder = FileSystem.Current.LocalStorage
+            let! folder = rootFolder.CreateFolderAsync("Pack", CreationCollisionOption.OpenIfExists) |> Async.AwaitTask
+            let! file = folder.CreateFileAsync("pack.zip", CreationCollisionOption.ReplaceExisting) |> Async.AwaitTask
+
+            use! stream = file.OpenAsync(FileAccess.ReadAndWrite) |> Async.AwaitTask
+            do! response.ResponseStream.CopyToAsync(stream) |> Async.AwaitTask
+        }
+        |> Async.RunSynchronously
+        |> ignore
         ()
