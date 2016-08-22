09c9997a9fa82e27fd5698af52ef2f29d6504fe0
Kai Ito <kai.ito@gmail.com>
2016-08-22 19:42:10 +0200

    Extract all contents from zip

--- Cursed.Base/Modpack.fs
+++ Cursed.Base/Modpack.fs
@@ -12,6 +12,55 @@ type State =
 
 type Modpack() =
     let mutable cursedState = { Link = ""; ZipLocation = "" }
+
+    let readBytes (stream: Stream) =
+        let memoryStream = new MemoryStream()
+        stream.CopyTo(memoryStream)
+        memoryStream.ToArray()
+
+    let extractFile (entry: ZipArchiveEntry) =
+        let rootFolder = FileSystem.Current.LocalStorage
+        let fullName = entry.FullName
+
+        if fullName.EndsWith("/") then
+            let numberOfDirs = 
+                fullName.ToCharArray()
+                |> Seq.filter (fun c -> c = '/')
+                |> Seq.length
+
+            let rec createDirs dirs (baseDir: IFolder) =
+                match dirs with
+                | [] -> ()
+                | head :: tail -> 
+                    if head = "" then
+                        ()
+                    else
+                        let dir = baseDir.CreateFolderAsync(head, CreationCollisionOption.OpenIfExists) |> Async.AwaitTask |> Async.RunSynchronously
+                        createDirs tail dir
+
+            match numberOfDirs with
+            | 1 -> createDirs [fullName] rootFolder
+            | _ -> createDirs (fullName.Split('/') |> List.ofArray) rootFolder
+        else
+            let filePath = fullName.Split('/') |> List.ofArray
+
+            let rec getBottomDir dirs (baseDir: IFolder) =
+                match dirs with
+                | [] -> baseDir
+                | head :: tail -> 
+                    let folder = baseDir.GetFolderAsync(head) |> Async.AwaitTask |> Async.RunSynchronously
+                    getBottomDir tail folder
+
+            let createFile (dir: IFolder) (entry: ZipArchiveEntry) =
+                let file = dir.CreateFileAsync(entry.Name, CreationCollisionOption.ReplaceExisting) |> Async.AwaitTask |> Async.RunSynchronously
+                use stream = file.OpenAsync(FileAccess.ReadAndWrite) |> Async.AwaitTask |> Async.RunSynchronously
+                let bytes = readBytes <| entry.Open()
+                do stream.WriteAsync(bytes, 0, bytes.Length) |> Async.AwaitTask |> Async.RunSynchronously
+
+            match filePath with
+            | [one] -> createFile rootFolder entry
+            | ls -> createFile (getBottomDir ls.[..ls.Length - 2] rootFolder) entry
+
     member this.UpdateState(state) =
         cursedState <- state
 
@@ -40,11 +89,7 @@ type Modpack() =
             use archive = new ZipArchive(stream)
             
             archive.Entries
-            |> Seq.iter (fun entry ->
-                let zipEntryFile = rootFolder.CreateFileAsync(entry.Name, CreationCollisionOption.ReplaceExisting) |> Async.AwaitTask |> Async.RunSynchronously
-                use outputStream = zipEntryFile.OpenAsync(FileAccess.ReadAndWrite) |> Async.AwaitTask |> Async.RunSynchronously
-                entry.Open().CopyToAsync(outputStream) |> Async.AwaitTask |> Async.RunSynchronously
-            )
+            |> Seq.iter extractFile
         }
         |> Async.RunSynchronously
         |> ignore
