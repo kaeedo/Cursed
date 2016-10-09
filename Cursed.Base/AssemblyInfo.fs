748e81aa56eb9ec073b02d3ea0d2cde58a268255
Kai Ito <kai.ito@gmail.com>
2016-10-09 17:51:29 +0200

    Move release notes into docs

--- Cursed.Base/AssemblyInfo.fs
+++ Cursed.Base/AssemblyInfo.fs
@@ -2,8 +2,8 @@
 namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyVersionAttribute("0.0.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] AssemblyVersion = "0.0.2"
+    let [<Literal>] AssemblyVersion = "0.0.3"
